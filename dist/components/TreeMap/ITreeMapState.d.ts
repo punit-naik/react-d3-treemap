b6271c0b29544056db0f702a9a34b87d21966f34
Marcus O'Mara <marcus@allstreet.org>
2021-03-31 11:48:16 +0200

    added dist

---
+++ dist/components/TreeMap/ITreeMapState.d.ts
@@ -0,0 +1,17 @@
+import { HierarchyRectangularNode } from "d3-hierarchy";
+export interface ITreeMapState {
+    scopedNodes: HierarchyRectangularNode<{}>[];
+    data: any;
+    width: number;
+    height: number;
+    xScaleFactor: number;
+    yScaleFactor: number;
+    xScaleFunction: any;
+    yScaleFunction: any;
+    zoomEnabled: boolean;
+    breadCrumbItems: any;
+    selectedId: any;
+    selectedNode: HierarchyRectangularNode<{}>;
+    totalNodes: number;
+    selectedNodeTotalNodes: number;
+}
---
+++ dist/containers/App.d.ts
@@ -0,0 +1,9 @@
+import * as React from "react";
+declare class App extends React.Component<{}, {
+    data: any;
+}> {
+    constructor(props: any);
+    render(): JSX.Element;
+}
+declare const _default: typeof App;
+export default _default;
